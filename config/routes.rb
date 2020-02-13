60ac87b45f24571a492c1020bdcc6e2c91dab270
Jorge Ramos <jorgeramos2068@gmail.com>
2020-02-13 08:47:05 -0700

    Add search functionality via AJAX

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Root
   root 'welcome#index'
+  # Devise routes
+  devise_for :users
+  # My Portfolio
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
